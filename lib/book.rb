62e04e7290c8e27e4282b8274bdc01ac40e50cb7
Eddington Rowellis <eddingtonibi@gmail.com>
2023-03-22 20:04:36 +0300

    commit tree 051dc776e6797992676e591ba8e578b6a82f8f42

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
 
+  attr_accessor :title, :author,  :page, :page_count, :genre 
+  def initialize(title)
+    @title = title
+    @author = author
+    @page = page
+    @page_count = page_count
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
 
